4252616c4b7f8d0dced97c8f0946b8263744e394
erick <etrujillov1@ucentral.edu.co>
2025-11-10 15:56:57 -0500

    Agregar script de build para Render

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # Instalar dependencias del sistema necesarias para FFmpeg y PyAV
-apt-get update && apt-get install -y ffmpeg libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libavfilter-dev libswscale-dev libswresample-dev
+apt-get update && apt-get install -y \
+    ffmpeg \
+    libavformat-dev \
+    libavcodec-dev \
+    libavdevice-dev \
+    libavutil-dev \
+    libavfilter-dev \
+    libswscale-dev \
+    libswresample-dev
 
 # Instalar dependencias de Python
 pip install -r requirements.txt
